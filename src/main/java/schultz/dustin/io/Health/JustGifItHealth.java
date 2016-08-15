6c1a07b42424f2760f22b6af166feed517b3d216
fpimentel <f.pimentel@gmail.com>
2016-08-15 19:22:24 -0400

    fisrt commit

---
+++ src/main/java/schultz/dustin/io/Health/JustGifItHealth.java
@@ -0,0 +1,27 @@
+package schultz.dustin.io.Health;
+
+import com.justgifit.JustGifItProperties;
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.HealthIndicator;
+import org.springframework.stereotype.Component;
+
+import javax.inject.Inject;
+
+/**
+ * Created by Fausto on 8/15/2016.
+ */
+@Component
+public class JustGifItHealth implements HealthIndicator {
+
+    @Inject
+    private JustGifItProperties properties;
+
+    @Override
+    public Health health() {
+        if(properties.getGifLocation().canWrite()) {
+            return Health.up().build();
+        }else{
+            return Health.down().build();
+        }
+    }
+}
